640d0fdc3714aea521edc8aaa82b78f7f7c27d71
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2021-07-13 23:45:44 -0400

    Add genio for testing purposes

--- src/gpio/gen.rs
+++ src/gpio/gen.rs
@@ -5,6 +5,20 @@ pub struct GenIoPin {
     pub pin_mode: PinMode,
 }
 
+impl GenIoPin {
+    fn set_low(&mut self) {
+        self.pin_state = false;
+    }
+
+    fn set_high(&mut self) {
+        self.pin_state = true;
+    }
+
+    fn toggle(&mut self) {
+        self.pin_state ^= true;
+    }
+}
+
 impl GPIO<GenIoPin> for Pin<GenIoPin> {
     fn init(pin_num: u8) -> Result<Pin<GenIoPin>, GPIOErr> {
         let genio = GenIoPin {
@@ -28,14 +42,17 @@ impl GPIO<GenIoPin> for Pin<GenIoPin> {
     }
 
     fn set_low(&mut self) -> Result<(), GPIOErr> {
+        self.pin_dev.set_low();
         Ok(())
     }
 
     fn set_high(&mut self) -> Result<(), GPIOErr> {
+        self.pin_dev.set_high();
         Ok(())
     }
 
     fn toggle(&mut self) -> Result<(), GPIOErr> {
+        self.pin_dev.toggle();
         Ok(())
     }
 }
@@ -45,9 +62,21 @@ mod tests {
     use crate::gpio::gen::GenIoPin;
     use crate::gpio::{Pin, GPIO};
 
+    fn setup(pin_num: u8) -> Pin<GenIoPin> {
+        let pin: Pin<GenIoPin> = Pin::init(pin_num).unwrap();
+        pin
+    }
+
     #[test]
     fn test_init() {
-        let pin: Pin<GenIoPin> = Pin::init(14).unwrap();
+        let pin: Pin<GenIoPin> = setup(14);
         assert_eq!(pin.pin_num(), 14);
+    }
+
+    #[test]
+    fn test_set_high() {
+        let mut pin: Pin<GenIoPin> = setup(14);
+        pin.set_high().unwrap();
+        assert_eq!(pin.is_high(), true);
     }
 }
