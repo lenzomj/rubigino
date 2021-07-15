e754be10e39bbf24e9c68245cae75bde5289b13e
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2021-07-15 04:11:54 +0100

    Add some basic rotate capability

--- examples/matrix.rs
+++ examples/matrix.rs
@@ -4,23 +4,23 @@ use std::{thread, time};
 fn main() {
     let delay = time::Duration::from_millis(500);
 
-    let mut max: MAX72XX = MAX72XX::init(8, 8).unwrap();
+    let mut max: MAX72XX = MAX72XX::init().unwrap();
     max.clear().unwrap();
 
-    let input: [u8; 8] = [
-        0b_10000010,
-        0b_01000001,
-        0b_10010010,
-        0b_01101001,
-        0b_10010010,
-        0b_01000001,
-        0b_10000010,
-        0b_01000001,
+    let mut input: [u8; 8] = [
+        0b_00000000,
+        0b_00011000,
+        0b_00011000,
+        0b_11111111,
+        0b_00011000,
+        0b_00011000,
+        0b_00011000,
+        0b_00011000,
     ];
 
-    max.write(&input).unwrap();
-
     loop {
+        max.write(&input).unwrap();
+        input = MAX72XX::rotate(&input);
         thread::sleep(delay);
     }
 }
