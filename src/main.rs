97714ada7744d7fb0293d657814e7a211b83fed6
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2021-07-20 01:43:20 +0100

    Add windowing scratch work

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use rubigino::display::font::Character;
+
+fn main() {
+
+    let input: String = format!(" { } ", "Hello");
+    let raw_chars: Vec<char>  = input.chars().rev().collect();
+    let chars: Vec<Character> = raw_chars
+        .iter()
+        .map(|c| Character::from(*c))
+        .collect();
+
+    for window in chars.windows(2) {
+    }
+
+
+
+}
+
+
+
